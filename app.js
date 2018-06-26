4226b4695258f739284b5ccd659fb20ddb355396
Camilo Snapp <camilo.snapp@gmail.com>
2018-06-25 21:46:37 -0600

    Added more general files in order to have required files to build app, Add normalize Add scripts files Created Views folder

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const debug = require('debug')('app');
 const path = require('path');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(morgan('tiny'));
+
 const port = process.env.PORT || 3000;
 app.set('port', process.env.PORT || 3000);
 
-
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/normalize')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  // __dirname = location of the current executable
+  res.sendFile(path.join(__dirname, '/public/views/index.html'));
 });
-
 
 app.listen(port, () => {
   debug(`listening on port ${(port)}`);
---
+++ public/js/scripts.js
@@ -0,0 +1,22 @@
+const generateColor = () => {
+  let palette = '#';
+  const chars = '0123456789ABCDEF';
+
+  for (let i = 0; i < 6; i++) {
+    palette += chars.charAt(Math.floor(Math.random() * 16));
+  }
+
+  return palette;
+};
+
+const generateFiveColors = () => {
+  const colorPalette = [];
+  for (let i = 0; i <= 4; i++) {
+    const hexColor = generateColor();
+    colorPalette.push(hexColor);
+  }
+  console.log(colorPalette)
+  return colorPalette;
+};
+
+$('.generator').on('click', generateFiveColors);
