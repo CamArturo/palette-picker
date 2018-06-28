8e22d4ff28c035a564f051b078b0b0363d517685
Camilo Snapp <camilo.snapp@gmail.com>
2018-06-27 20:42:20 -0600

    Refactor palette generator

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,35 +1,77 @@
-const generateColor = () => {
-  let palette = '#';
+// const generateColor = () => {
+//   let palette = '#';
+//   const chars = '0123456789ABCDEF';
+//
+//   for (let i = 0; i < 6; i++) {
+//     palette += chars.charAt(Math.floor(Math.random() * 16));
+//   }
+//
+//   return palette;
+// };
+
+let colorPalette = [];
+
+const removePreviousColorPalette = () => {
+  colorPalette = [];
+};
+
+const generateHexColor = () => {
+  let hexColor = '#';
   const chars = '0123456789ABCDEF';
 
   for (let i = 0; i < 6; i++) {
-    palette += chars.charAt(Math.floor(Math.random() * 16));
+    hexColor += chars.charAt(Math.floor(Math.random() * 16));
   }
-
-  return palette;
+  return hexColor;
 };
 
-const generateFiveColors = () => {
-  const colorPalette = [];
-  for (let i = 0; i <= 4; i++) {
-    const hexColor = generateColor();
+const generateColors = () => {
+  let colorAmount = $('.card-container button.open');
+  colorAmount = colorAmount.length;
+
+  for (let i = 0; i < colorAmount; i++) {
+    const hexColor = generateHexColor();
     colorPalette.push(hexColor);
   }
-  console.log(colorPalette);
   setCSS(colorPalette);
+  removePreviousColorPalette();
+};
+
+const grabColor = () => {
+  let color = colorPalette
 };
 
 const setCSS = (colorPalette) => {
-  $('.card-1').css('background-color', colorPalette[0]);
-  $(`<p>${colorPalette[0]}</p>`).appendTo($('.card-1 .card-bottom'));
-  $('.card-2').css('background-color', colorPalette[1]);
-  $(`<p>${colorPalette[1]}</p>`).appendTo($('.card-2 .card-bottom'));
-  $('.card-3').css('background-color', colorPalette[2]);
-  $(`<p>${colorPalette[2]}</p>`).appendTo($('.card-3 .card-bottom'));
-  $('.card-4').css('background-color', colorPalette[3]);
-  $(`<p>${colorPalette[3]}</p>`).appendTo($('.card-4 .card-bottom'));
-  $('.card-5').css('background-color', colorPalette[4]);
-  $(`<p>${colorPalette[4]}</p>`).appendTo($('.card-5 .card-bottom'));
+
+  if (!$('card-1 button').hasClass('open')) {
+    $('.card-1 .card-bottom p').remove();
+    $('.card-1 .card').css('background-color', colorPalette[0]);
+    $(`<p>${colorPalette[0]}</p>`).appendTo($('.card-1 .card-bottom'));
+  }
+
+  if (!$('card-2 button').hasClass('open')) {
+    $('.card-2 .card-bottom p').remove();
+    $('.card-2 .card').css('background-color', colorPalette[1]);
+    $(`<p>${colorPalette[1]}</p>`).appendTo($('.card-2 .card-bottom'));
+  }
+
+  if (!$('card-3 button').hasClass('open')) {
+    $('.card-3 .card-bottom p').remove();
+    $('.card-3 .card').css('background-color', colorPalette[2]);
+    $(`<p>${colorPalette[2]}</p>`).appendTo($('.card-3 .card-bottom'));
+  }
+
+  if (!$('card-4 button').hasClass('open')) {
+    $('.card-4 .card-bottom p').remove();
+    $('.card-4 .card').css('background-color', colorPalette[3]);
+    $(`<p>${colorPalette[3]}</p>`).appendTo($('.card-4 .card-bottom'));
+  }
+
+  if (!$('card-5 button').hasClass('open')) {
+    $('.card-5 .card-bottom p').remove();
+    $('.card-5 .card').css('background-color', colorPalette[4]);
+    $(`<p>${colorPalette[4]}</p>`).appendTo($('.card-5 .card-bottom'));
+  }
 };
 
 const createProject = () => {
@@ -49,16 +91,19 @@ const createProject = () => {
     .catch(error => console.log(error));
 };
 
-$('.generator-btn').on('click', generateFiveColors);
+// generateColors
+//
+
+$('.generator-btn').on('click', generateColors);
 $('.lock-btn').click(function () {
   $(this).toggleClass('open');
 });
 
 $('.create-project').on('click', createProject);
 
-$(function() {
+$(function () {
   fetch('/api/v1/projects', {
-    method: 'GET',
+    method: 'GET'
   })
     .then(response => response.json())
     .then(response => response.forEach(project => {
