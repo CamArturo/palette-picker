ba9ef45b86b185ed70308d13e9c93a8342edc0f5
Camilo Snapp <camilo.snapp@gmail.com>
2018-07-05 10:18:32 -0600

    delete comment

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 3000;
 app.set('port', process.env.PORT || 3000);
 
-// if (process.env.NODE_ENV !== 'test') {
-//   app.use(logger('dev'));
-// }
-
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/normalize')));
